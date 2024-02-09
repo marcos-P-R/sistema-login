4cfca324a196af7b5b6dd137aa79fee541dd17c0
marcos.p <marcos.p.mcruz@gmail.com>
2024-02-09 19:31:07 -0300

    Add mensagems de retorno da api

---
+++ src/enum/messageResponse.ts
@@ -0,0 +1,7 @@
+export enum messageResponse {
+  NOT_FOUND_EMAIL = 'Email não encontrado',
+  SUCESS_AUTH = 'Sucess login',
+  FAILED_AUTH = 'Failed login',
+  PASSWORD_AUTH_FAILED = 'Senha incorreta',
+  FAILED_TOKEN = 'Failed to authenticate token'
+}
